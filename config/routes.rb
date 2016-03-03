a2a07b01e42b590308c530421f45741b2f20acba
Justin <justin@justb.uk>
2016-03-03 13:35:50 +0000

    Added a user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
  
+  get 'users/show'
+
   devise_for :users, controllers: {registrations: 'registrations'}
 
   get 'pages/home'
   root 'pages#home'
+
+  resources :users, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
