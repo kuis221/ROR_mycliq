4f5a848d16899b04f4a550c1362b9a3c64813577
Justin <justin@justb.uk>
2016-03-03 20:20:11 +0000

    Finishing touches before production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,34 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!, only:[:index]
+  before_action :authenticate_user!, only:[:index]
 	before_action :set_user, only:[:show]
+  before_action :get_counts, only: [:index]
 
 	def index
-    	case params[:people] when "friends"
-          @users = current_user.active_friends
-        when "requests"
-          @users = current_user.pending_friend_requests_from.map(&:user)
-        when "pending"
-          @users = current_user.pending_friend_requests_to.map(&:friend)
-        else
-          @users = User.where.not(id: current_user.id)
-        end
+		case params[:people] when "friends"
+      @users = current_user.active_friends
+    when "requests"
+      @users = current_user.pending_friend_requests_from.map(&:user)
+    when "pending"
+      @users = current_user.pending_friend_requests_to.map(&:friend)
+    else
+      @users = User.where.not(id: current_user.id)
+    end
 	end
 
   def show
-  	    @post = Post.new
+    @post = Post.new
     @posts = @user.posts.order('created_at DESC')
-  	 @activities = PublicActivity::Activity.where(owner_id: @user.id)
+    @activities = PublicActivity::Activity.where(owner_id: @user.id).order('created_at DESC')
   end
 
   private
 
+  def get_counts
+    @friend_count = current_user.active_friends.size
+    @pending_count = current_user.pending_friend_requests_to.map(&:friend).size
+  end
+
   def set_user
   	@user = User.find_by(username: params[:id])
   end
-  
 end
