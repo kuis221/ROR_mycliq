d063295c71670fcacd8556fcd01413cc82add89e
Justin <justin@justb.uk>
2016-03-03 14:52:39 +0000

    Added Friendship status detection

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,20 @@ def pending_friend_requests_to
     self.friendships.where(state: "active").map(&:friend) + self.inverse_friendships.where(state: "active").map(&:user)
   end
 
+  def friendship_status(user_2)
+    friendship = Friendship.where(user_id: [self.id,user_2.id], friend_id: [self.id,user_2.id])
+    unless friendship.any?
+      return "not_friends"
+    else
+      if friendship.first.state == "active"
+        return "friends"
+      else
+        if friendship.first.user == self
+          return "pending"
+        else
+          return "requested"
+        end
+      end
+    end
+  end
 end
--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class FrienshipTest < ActiveSupport::TestCase
+class FriendshipTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
