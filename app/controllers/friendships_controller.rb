a55f93f570fa4954f6b1989b6a5abf24c9599381
Justin <justin@justb.uk>
2016-03-03 15:20:49 +0000

    Friendships active

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,38 @@
+class FriendshipsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :set_user, only: [:create]
+	before_action :set_friendship, only: [:destroy, :accept]
+
+	def create
+		@friendship = current_user.request_friendship(@user)
+		respond_to do |format|
+			format.html {redirect_to users_path, notice: "Friendship Created"}
+		end
+	end
+
+	def destroy
+		@friendship.destroy
+		respond_to do |format|
+			format.html {redirect_to users_path, notice: "Friendship Deleted"}
+		end
+	end
+
 	def accept
 		@friendship.accept_friendship
+		@friendship.create_activity key: 'friendship.accepted', owner: @friendship.user, recipient: @friendship.friend
+		@friendship.create_activity key: 'friendship.accepted', owner: @friendship.friend, recipient: @friendship.user
 		respond_to do |format|
 			format.html {redirect_to users_path, notice: "Friendship Accepted"}
 		end
 	end
+
+	private
+
+	def set_user
+		@user = User.find(params[:user_id])
+	end
+
+	def set_friendship
+		@friendship = Friendship.find(params[:id])
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,10 @@ class User < ActiveRecord::Base
 
   has_many :friendships, dependent: :destroy
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+  has_many :posts, dependent: :destroy
 
-    def request_friendship(user_2)
+
+  def request_friendship(user_2)
   	self.friendships.create(friend: user_2)
   end
 
@@ -18,7 +20,7 @@ class User < ActiveRecord::Base
     self.inverse_friendships.where(state: "pending")
   end
 
-def pending_friend_requests_to
+  def pending_friend_requests_to
     self.friendships.where(state: "pending")
   end
 
@@ -42,7 +44,8 @@ def pending_friend_requests_to
       end
     end
   end
-    def friendship_relation(user_2)
+
+  def friendship_relation(user_2)
     Friendship.where(user_id: [self.id,user_2.id], friend_id: [self.id,user_2.id]).first
   end
 end
